7263d83729f06de1650f28d4fb86aa6ad263c157
Pjotr Prins <pjotr.public01@thebird.nl>
2011-02-11 13:51:23 +0100

    Splitter

--- lib/bio/db/blast/xmlparser.rb
+++ lib/bio/db/blast/xmlparser.rb
@@ -1,16 +1,35 @@
+# require 'nokogiri'
+
 module Bio
   module Blast
     # Reads a full XML result and splits it out into a buffer for each
     # Iteration (query result).
     class XmlBulkParser
+      # include Nokogiri
       def initialize fn
         @fn = fn
       end
 
       def each
-        File.open(@fn) do | f |
-          f.each_line do | line | 
-            # p line
+        f = File.open(@fn)
+        f.each_line do | line |
+          break if line.strip == "<Iteration>"
+        end
+        each_iteration(f) do | b |
+          # input = Nokogiri::XML(b.join)
+          yield b
+        end
+      end
+
+    private
+
+      def each_iteration f
+        b = ["<Iteration>"]
+        f.each_line do | line |
+          b << line
+          if line.strip == "</Iteration>"
+            yield b
+            b = []
           end
         end
       end
--- spec/bio-blastxmlparser_spec.rb
+++ spec/bio-blastxmlparser_spec.rb
@@ -1,10 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe Bio::Blast::XmlBulkParser do
+describe Bio::Blast::XmlSplitter do
   include Bio::Blast
   it "should read a large file and yield Iterations" do
-    p = XmlBulkParser.new("./test/data/nt_example_blastn.m7")
+    p = XmlSplitter.new("./test/data/nt_example_blastn.m7")
     p.each do | result |
+      result[1].to_s.should =~ /Iteration_iter-num/
     end
   end
 end
