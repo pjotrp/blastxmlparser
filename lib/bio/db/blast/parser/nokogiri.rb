d041246da48c0070e9031d7f82effdf97258c6fd
Pjotr Prins <pjotr.public01@thebird.nl>
2011-02-12 10:33:27 +0100

    Reaping Hsp info

--- lib/bio/db/blast/parser/nokogiri.rb
+++ lib/bio/db/blast/parser/nokogiri.rb
@@ -12,7 +12,7 @@ module Bio
 
     end
 
-    # Some magic to creat XML -> method mappers, on the fly
+    # Some magic to create XML -> method mappers, on the fly
     module MapXPath
       include XPath
       def MapXPath.define_s map
@@ -29,18 +29,71 @@ module Bio
           }
         }
       end
+      def MapXPath.define_f map
+        map.each { |k,v| 
+          define_method(v) {
+            field(k).to_f
+          }
+        }
+      end
+    end
+
+    class NokogiriBlastHsp
+      include MapXPath
+      MapXPath.define_s 'Hsp_id'          => :hsp_id,
+                        'Hsp_qseq'        => :qseq,
+                        'Hsp_hseq'        => :hseq,
+                        'Hsp_midline'     => :midline
+      MapXPath.define_i 'Hsp_num'         => :hsp_num,
+                        'Hsp_score'       => :score,
+                        'Hsp_query-from'  => :query_from,
+                        'Hsp_query-to'    => :query_to,
+                        'Hsp_hit-from'    => :hit_from,
+                        'Hsp_hit-to'      => :hit_to,
+                        'Hsp_query-frame' => :query_frame,
+                        'Hsp_hit-frame'   => :hit_frame,
+                        'Hsp_identity'    => :identity,
+                        'Hsp_positive'    => :positive,
+                        'Hsp_align-len'   => :align_len
+      MapXPath.define_f 'Hsp_bit-score'   => :bit_score,
+                        'Hsp_evalue'      => :evalue
+                         
+      def initialize xml
+        @xml = xml
+      end
+
+      def to_s
+        s = <<EOM
+Hsp: id=#{hsp_id}, score=#{score}, bit_score=#{bit_score}
+EOM
+      end
+      
     end
 
     class NokogiriBlastHit
       include MapXPath
-      MapXPath.define_s 'Hit_id' => :id,
-                        'Hit_def' => :def,
+      MapXPath.define_s 'Hit_id' => :hit_id,
+                        'Hit_def' => :hit_def,
                         'Hit_accession' => :accession
-      MapXPath.define_i 'Hit_num' => :num, 
+      MapXPath.define_i 'Hit_num' => :hit_num, 
                         'Hit_len' => :len
 
       def initialize xml
         @xml = xml
+      end
+      
+      def hsps
+        Enumerator.new { |yielder|
+          @xml.xpath("//Hsp").each { | hsp |
+            yielder.yield NokogiriBlastHsp.new(hsp)
+          }
+        }
+      end
+
+      def to_s
+        s = <<EOM
+Hit: id=#{hit_id}, hit_def=#{hit_def}, hit_num=#{hit_num}
+EOM
       end
       
     end
--- spec/bio-blastxmlparser_spec.rb
+++ spec/bio-blastxmlparser_spec.rb
@@ -23,17 +23,47 @@ describe "Bio::Blast::NokogiriBlastXml" do
     @iter1.query_id.should == "lcl|1_0"
     @iter1.query_def.should == "I_1 [477 - 884] "
     @iter1.query_len.should == 408
+    @iter2.iter_num.should == 2
   end
   it "should support Hit fields" do
     hit = @iter1.hits.first
-    hit.num.should == 1
-    hit.id.should == "lcl|I_74685"
-    hit.def.should == "[57809 - 57666] (REVERSE SENSE) "
+    hit.hit_num.should == 1
+    hit.hit_id.should == "lcl|I_74685"
+    hit.hit_def.should == "[57809 - 57666] (REVERSE SENSE) "
     hit.accession.should == "I_74685"
     hit.len.should == 144
   end
   it "should iterate Hsps in a hit" do
     hsp = @iter1.hits.first.hsps.first
+    hsp.hsp_num.should == 1
+    hsp.bit_score.should == 145.205
+    hsp.score.should == 73
+    hsp.evalue.should == 5.82208e-34
+    hsp.query_from.should == 28
+    @iter2.iter_num.should == 2
+    hits = @iter2.hits.first(3)
+    hits.each do | hit |
+      p hit
+    end
+    hsps.each do | hsp |
+      p hsp
+    end
+    hsps[1].bit_score.should == 69.8753
+    #           <Hsp_query-from>28</Hsp_query-from>
+    #           <Hsp_query-to>100</Hsp_query-to>
+    #          <Hsp_hit-from>28</Hsp_hit-from>
+    #          <Hsp_hit-to>100</Hsp_hit-to>
+    #          <Hsp_query-frame>1</Hsp_query-frame>
+    #          <Hsp_hit-frame>1</Hsp_hit-frame>
+    #          <Hsp_identity>73</Hsp_identity>
+    #          <Hsp_positive>73</Hsp_positive>
+    #          <Hsp_align-len>73</Hsp_align-len>
+    #          <Hsp_qseq>AGTGAAGCTTCTAGATATTTGGCGGGTACCTCTAATTTTGCCTGCCTGCCAACCTA
+#CTCCTGTGTTTAG</Hsp_qseq>
+    #          <Hsp_hseq>AGTGAAGCTTCTAGATATTTGGCGGGTACCTCTAATTTTGCCTGCCTGCCAACCTA
+#CTCCTGTGTTTAG</Hsp_hseq>
+    #          <Hsp_midline>|||||||||||||||||||||||||||||||||||||||||||||||||||||
+
   end
   it "should support Hsp fields"
 end
